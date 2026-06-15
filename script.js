f8e14947ca5388329f9c4b8d6bc942ccb22e2ead
nonymea42-afk <nonyme.a42@gmail.com>
2026-06-15 15:14:34 +0200

    Refactor and clean up script.js for readability

--- script.js
+++ script.js
@@ -41,14 +41,14 @@ let rotation = 0;
 let spinning = false;
 
 const defaults = [
-    "Read",
-    "Study",
-    "Movie",
-    "Journal",
-    "Coffee",
-    "Music",
-    "Walk",
-    "Nap"
+    "Label 1",
+    "Label 2",
+    "Label 3",
+    "Label 4",
+    "Label 5",
+    "Label 6",
+    "Label 7",
+    "Label 8"
 ];
 
 let labels =
